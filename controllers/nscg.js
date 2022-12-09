45a06d40a422362036906111c73f306a13423ad6
Joseph Emrick <josephemrick@Josephs-MacBook-Air.local>
2022-12-09 07:11:30 -0800

    added INDUCES routes

--- controllers/nscg.js
+++ controllers/nscg.js
@@ -27,15 +27,56 @@ function errorHandler(error, res) {
 /// SEED ///
 router.get('/seed', (req, res) => {
 
-})
+});
 
 
 
 /// INDEX ///
-router.get('/', (req, res) => {
-    res.render('ncsg/index.ejs')
-})
+router.get('/',  async (req, res) => {
+    const decks = await Deck.find({}).catch((error) => errorhandler (error, res))
+    res.render('ncsg/login.ejs', {decks})
+});
 
+
+/// NEW ///
+router.get('/new', (req, res) => {
+    res.render('ncsg/new.ejs')
+});
+
+
+/// DESTROY ///
+router.delete('/:id', async (req, res) => {
+    await Deck.findByIdAndRemove(req.params.id).catch((error) => errorHandler (error, res))
+    res.redirect('/nscg')
+});
+
+
+/// UPDATE ///
+router.put('/:id', async (req, res) => {
+    await Deck.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect('/uscg')
+});
+
+
+/// CREATE ///
+router.post('/', async (req, res) => {
+    await Deck.create(req.body).catch((error) => errorHandler(error, res))
+    res.redirect('/uscg')
+});
+
+
+/// EDIT ///
+router.get('/:id/edit', async (req, res) => {
+    const decks = await Deck.findById(req.params.id).catch((error) => errorHandler(error, res))
+        res.render('uscg/edit.ejs', {decks})
+});
+
+
+/// SHOW ///
+router.get('/:id', async (req, res) => {
+    const decks = await Deck.findById(req.params.id).catch((error) => errorHandler(error, res))
+    res.render('uscg/show.ejs', {decks})
+});
 
 
 ///////////////////////////////////////////////
